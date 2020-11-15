1a377eaa0280d6f22da0741e220355c16e871003
unknown <tiago.peterson@gmail.com>
2020-11-14 23:11:52 -0300

    First Commit

---
+++ SCA.Ativos.API/Controllers/AtivoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SCA.Ativos.API.Model;
+
+namespace SCA.Ativos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AtivoController : ControllerBase
+    {
+        private readonly ILogger<AtivoController> _logger;
+
+        public AtivoController(ILogger<AtivoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ativo> Get()
+        {
+            return new List<Ativo>();
+        }
+    }
+}
---
+++ SCA.Ativos.API/Model/Ativo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCA.Ativos.API.Model
+{
+    public class Ativo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public string Identificador { get; set; }
+        public string DataCompra { get; set; }
+        public string DataCadastro { get; set; }
+        public string DataUltimaManutencao { get; set; }
+        public string Categoria { get; set; }
+        public string Fornecedor { get; set; }
+    }
+}
